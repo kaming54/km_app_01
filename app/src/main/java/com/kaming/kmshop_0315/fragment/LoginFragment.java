5dd89c20ce779bfbc15d7bb55dea6f6c52e6792b
kaming54 <km20170101@gmail.com>
2019-03-25 18:07:57 +0800

    連左Firebase

--- app/src/main/java/com/kaming/kmshop_0315/fragment/LoginFragment.java
+++ app/src/main/java/com/kaming/kmshop_0315/fragment/LoginFragment.java
@@ -3,7 +3,6 @@ package com.kaming.kmshop_0315.fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,18 +13,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.kaming.kmshop_0315.R;
 
-public class MeMenuFragment extends Fragment {
+public class LoginFragment {
 
-    private FirebaseAuth mAuth;
-    @Nullable
-    @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        mAuth = FirebaseAuth.getInstance();
-        FirebaseUser user = mAuth.getCurrentUser();
-        if(user==null) {
             return inflater.inflate(R.layout.fragment_login, container, false);
-        }else {
-            return inflater.inflate(R.layout.fragment_menu_me, container, false);
-        }
     }
 }
